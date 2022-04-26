d9a44791fcf3da28a1ed151d32b4bd97c9653df1
Yrii Parhom <parhomenko.yurij@gmail.com>
2022-04-26 20:47:08 +0300

    reformat code

--- src/Worker.java
+++ src/Worker.java
@@ -2,15 +2,15 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener error;
 
-    public Worker(OnTaskDoneListener callback, OnTaskErrorListener error){
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener error) {
         this.callback = callback;
         this.error = error;
     }
 
-    public void start(){
-        for (int i = 1; i <= 100; i++){
+    public void start() {
+        for (int i = 1; i <= 100; i++) {
             callback.onDone("Task " + i + " is done");
-           if (i == 33) error.onError("Task " + i + " error");
+            if (i == 33) error.onError("Task " + i + " error");
         }
     }
 }
