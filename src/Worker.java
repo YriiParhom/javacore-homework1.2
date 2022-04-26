96793c4aef69dc3008a409daf8a23a5af1148f86
Yrii Parhom <parhomenko.yurij@gmail.com>
2022-04-26 20:42:47 +0300

    add new files

---
+++ src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener error;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener error){
+        this.callback = callback;
+        this.error = error;
+    }
+
+    public void start(){
+        for (int i = 1; i <= 100; i++){
+            callback.onDone("Task " + i + " is done");
+           if (i == 33) error.onError("Task " + i + " error");
+        }
+    }
+}
